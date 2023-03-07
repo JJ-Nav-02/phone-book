da33f3cda8475353c0b2bc8ee6828083b38c3261
Joanna de Guadalupe Jiménez Vázquez <124840849+JJ-Nav-02@users.noreply.github.com>
2023-03-07 00:05:29 -0600

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,73 +1,84 @@
-import React, {useState} from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom/client';
 import Personas from './componentes/personas';
 import Filtro from './componentes/filtro';
 import Formulario from './componentes/formulario';
+import axios from 'axios';
+
 
 
 const App = () => {
   const [persons, setPersons] = useState([])
-  const [newPerson, setNewPerson] = useState({name: '', phone: ''})
+  const [newPerson, setNewPerson] = useState({ name: '', phone: '' })
   const [filter, setFilter] = useState('')
   const isNewName = persons.find(
     (persons) =>
-    persons.name.toLocaleLowerCase()=== newPerson.name.toLocaleLowerCase()
+      persons.name.toLocaleLowerCase() === newPerson.name.toLocaleLowerCase()
   )
 
-  const addNewName = (event) =>{
+  useEffect(() => {
+    console.log('Entro al Effect');
+    axios.get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('Entro al then');
+        setPersons(response.data)
+      })
+  }, [])
+  
+  const addNewName = (event) => {
     event.preventDefault()
 
-    if(!isNewName){
+    if (!isNewName) {
       const personObject = {
         name: newPerson.name,
         phone: newPerson.phone,
       }
 
       setPersons(persons.concat(personObject))
-      setNewPerson({name: '',phone: ''})
+      setNewPerson({ name: '', phone: '' })
     } else {
       alert(`${newPerson.name} ya esta dado de alta`)
     }
   }
 
   const handleNewName = (event) =>
-    setNewPerson({newPerson,name: event.target.value})
+    setNewPerson({ newPerson, name: event.target.value })
 
   const handleNewPhone = (event) =>
-    setNewPerson({newPerson, phone: event.target.value})
+    setNewPerson({ newPerson, phone: event.target.value })
 
-  const handleFilter= (event) => setFilter(event.target.value)
+  const handleFilter = (event) => setFilter(event.target.value)
 
-  const personasMostrar = 
+  const personasMostrar =
     filter === ''
-    ? persons
-    : persons.filter(
-      (person) =>
-      person.name
-      .toLocaleLowerCase()
-      .indexOf(filter.toLocaleLowerCase()) > -1
-    )
+      ? persons
+      : persons.filter(
+        (person) =>
+          person.name
+            .toLocaleLowerCase()
+            .indexOf(filter.toLocaleLowerCase()) > -1
+      )
 
-    
 
-    return(
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <Filtro handleFilter={handleFilter} />
+
       <div>
-        <h2>Phonebook</h2>
-        <Filtro handleFilter={handleFilter} />
-
-        <div>
-          <h2>add a new</h2>
-        </div>
-        <h2>Numbers</h2>
-        <Formulario
+        <h2>add a new</h2>
+      </div>
+      <h2>Numbers</h2>
+      <Formulario
         addNewName={addNewName}
         handleNewName={handleNewName}
         handleNewPhone={handleNewPhone}
         newPerson={newPerson}
-        />
-        <Personas personasMostrar={personasMostrar} />
-      </div>
-    )
+      />
+      <Personas personasMostrar={personasMostrar} />
+    </div>
+  )
 
 }
 
